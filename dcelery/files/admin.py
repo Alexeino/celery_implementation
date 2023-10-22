1234d23e4b253955784dc4afd6727389ea2f9da7
alexeino <ms8750122327@gmail.com>
2023-10-22 12:53:21 +0530

    Django App - Upload Files DRF

---
+++ dcelery/files/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import File
+# Register your models here.
+
+
+class FileAdmin(admin.ModelAdmin):
+    list_display = ["file","uploaded_at"]
+    class Meta:
+        model = File
+        
+        
+admin.site.register(File,FileAdmin)
